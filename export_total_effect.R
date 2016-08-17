3b7606b26af838909279df483734271ff52fa79c
Frank Blaauw <frank.blaauw@gmail.com>
2016-08-17 15:01:03 +0200

    Also export the positive and negative of irf graphs

--- export_total_effect.R
+++ export_total_effect.R
@@ -34,7 +34,8 @@
 
     for (source in rownames(net$network)) {
       for (target in colnames(net$network)) {
-        weight<- as.numeric(net$network[source, target])
+        weight <- as.numeric(net$network[source, target])
+        label  <- ifelse(weight >= 0, 'positive', 'negative')
 
         # Retrieve the the current standard deviation for the source / target
         # values
@@ -43,6 +44,14 @@
         index_cur <- min(NonNAindex_cur)
         cur[[1]][index_cur] <- weight
         result_matrices[[group]][['total-sd']][source, target] <- cur
+
+        # Retrieve the positive and negative values
+        result_matrices[[group]][[label]][source, target] <- cur
+        cur <- result_matrices[[group]][[label]][source, target]
+        NonNAindex_cur <- which(is.na(cur[[1]]))
+        index_cur <- min(NonNAindex_cur)
+        cur[[1]][index_cur] <- weight
+        result_matrices[[group]][[label]][source, target] <- cur
 
         cur_total <- result_matrices$total[['total-sd']][source, target]
         NonNAindex_total <- which(is.na(cur_total[[1]]))
@@ -98,7 +107,6 @@
   qgraph(result_matrices$anhedonia$total / length(anhedonia),  vsize=4.5, edge.labels = TRUE, minimum = glob_minimum, maximum = maximum, layout=layout, groups=groups,posCol="chartreuse3",labels=labels,title="Anhedonia total effect summed, and averaged")#,nodeNames=bdinms2,legend.cex=0.6)
   qgraph(result_matrices$no_anhedonia$total / length(no_anhedonia), vsize=4.5, edge.labels = TRUE, minimum = glob_minimum, maximum = maximum,  layout=layout,groups=groups, posCol="chartreuse3",labels=labels,title="No Anhedonia total effect summed, and average")#,nodeNames=bdinms2,legend.cex=0.6)
 
-
   plottable_sd_anhedonia <- .create_standard_deviation_matrix(result_matrices$anhedonia[['total-sd']], labels)
   plottable_sd_no_anhedonia <- .create_standard_deviation_matrix(result_matrices$no_anhedonia[['total-sd']], labels)
   minimum = abs(min(plottable_sd_anhedonia, plottable_sd_no_anhedonia))
@@ -136,11 +144,15 @@
   table_result <- data.frame()
   anhedonia_count_column <- 'anhedonia_count'
   anhedonia_column <- 'anhedonia'
+  anhedonia_positive_column <- 'anhedonia_positive'
+  anhedonia_negative_column <- 'anhedonia_negative'
   anhedonia_average_column <- 'anhedonia_average'
   anhedonia_sd_column <- 'anhedonia_sd'
 
   no_anhedonia_count_column <- 'no-anhedonia_count'
   no_anhedonia_column <- 'no-anhedonia'
+  no_anhedonia_positive_column <- 'no-anhedonia_positive'
+  no_anhedonia_negative_column <- 'no-anhedonia_negative'
   no_anhedonia_average_column <- 'no-anhedonia_average'
   no_anhedonia_sd_column <- 'no-anhedonia_sd'
 
@@ -151,11 +163,15 @@
 
   table_result['x', anhedonia_column] <- 0
   table_result['x', anhedonia_count_column] <- 0
+  table_result['x', anhedonia_positive_column] <- 0
+  table_result['x', anhedonia_negative_column] <- 0
   table_result['x', anhedonia_average_column] <- 0
   table_result['x', anhedonia_sd_column] <- 0
 
   table_result['x', no_anhedonia_column] <- 0
   table_result['x', no_anhedonia_count_column] <- 0
+  table_result['x', no_anhedonia_positive_column] <- 0
+  table_result['x', no_anhedonia_negative_column] <- 0
   table_result['x', no_anhedonia_average_column] <- 0
   table_result['x', no_anhedonia_sd_column] <- 0
 
@@ -170,20 +186,28 @@
 
       # Anhedonia
       an_total <- result_matrices$anhedonia$total[source, target]
+      an_positive <- result_matrices$anhedonia$positive[source, target]
+      an_negative <- result_matrices$anhedonia$negative[source, target]
       an_total_sd <- result_matrices$anhedonia[['total-sd']][[source, target]]
       an_total_sd <- an_total_sd[!is.na(an_total_sd) & an_total_sd != 0]
       table_result[row_name, anhedonia_column] <-  an_total
       table_result[row_name, anhedonia_count_column] <- length(an_total_sd)
       table_result[row_name, anhedonia_sd_column] <- sd(an_total_sd)
+      table_result[row_name, anhedonia_positive_column] <-  an_positive
+      table_result[row_name, anhedonia_negative_column] <-  an_negative
       table_result[row_name, anhedonia_average_column] <- mean(an_total_sd)
 
       # No-Anhedonia
       no_an_total <- result_matrices$no_anhedonia$total[source, target]
+      no_an_positive <- result_matrices$no_anhedonia$positive[source, target]
+      no_an_negative <- result_matrices$no_anhedonia$negative[source, target]
       no_an_total_sd <- result_matrices$no_anhedonia[['total-sd']][[source, target]]
       no_an_total_sd <- no_an_total_sd[!is.na(no_an_total_sd) & no_an_total_sd != 0]
       table_result[row_name, no_anhedonia_column] <-  no_an_total
       table_result[row_name, no_anhedonia_count_column] <- length(no_an_total_sd)
       table_result[row_name, no_anhedonia_sd_column] <- sd(no_an_total_sd)
+      table_result[row_name, no_anhedonia_positive_column] <-  no_an_positive
+      table_result[row_name, no_anhedonia_negative_column] <-  no_an_negative
       table_result[row_name, no_anhedonia_average_column] <- mean(no_an_total_sd)
 
       # Total
@@ -202,7 +226,7 @@
 export_total_effect_networks <- function(airas,general_groups) {
   networks_output <- mclapply(airas, .generate_total_effect_network, mc.cores = detectCores())
   groups <- c('other', 'anhedonia', 'no_anhedonia', 'total')
-  options <- c('total', 'total-sd')
+  options <- c('total', 'total-sd', 'positive', 'negative')
   names <- dimnames(airas[[1]]$var_model$y)[[2]]
 
   # Retrieve the number of variables
