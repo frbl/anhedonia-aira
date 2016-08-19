846b6c773776562c982047d7bbe35dd1799b9f32
Frank Blaauw <frank.blaauw@gmail.com>
2016-08-19 09:47:27 +0200

    Added a field showing the count of positive / negative edges

--- export_total_effect.R
+++ export_total_effect.R
@@ -35,7 +35,6 @@
     for (source in rownames(net$network)) {
       for (target in colnames(net$network)) {
         weight <- as.numeric(net$network[source, target])
-        label  <- ifelse(weight >= 0, 'positive', 'negative')
 
         # Retrieve the the current standard deviation for the source / target
         # values
@@ -46,8 +45,14 @@
         result_matrices[[group]][['total-sd']][source, target] <- cur
 
         # Retrieve the positive and negative values
-        cur_label <- result_matrices[[group]][[label]][source, target]
-        result_matrices[[group]][[label]][source, target] <- cur_label + weight
+        if(weight != 0) {
+          label  <- ifelse(weight > 0, 'positive', 'negative')
+          label_count <- paste(label,'count',sep='_')
+          cur_label <- result_matrices[[group]][[label]][source, target]
+          result_matrices[[group]][[label]][source, target] <- cur_label + weight
+          cur_label_count <- result_matrices[[group]][[label_count]][source, target]
+          result_matrices[[group]][[label_count]][source, target] <- cur_label_count + 1
+        }
 
         cur_total <- result_matrices$total[['total-sd']][source, target]
         NonNAindex_total <- which(is.na(cur_total[[1]]))
@@ -141,14 +146,18 @@
   anhedonia_count_column <- 'anhedonia_count'
   anhedonia_column <- 'anhedonia'
   anhedonia_positive_column <- 'anhedonia_positive'
+  anhedonia_positive_count_column <- 'anhedonia_positive_count'
   anhedonia_negative_column <- 'anhedonia_negative'
+  anhedonia_negative_count_column <- 'anhedonia_negative_count'
   anhedonia_average_column <- 'anhedonia_average'
   anhedonia_sd_column <- 'anhedonia_sd'
 
   no_anhedonia_count_column <- 'no-anhedonia_count'
   no_anhedonia_column <- 'no-anhedonia'
   no_anhedonia_positive_column <- 'no-anhedonia_positive'
+  no_anhedonia_positive_count_column <- 'no-anhedonia_positive_count'
   no_anhedonia_negative_column <- 'no-anhedonia_negative'
+  no_anhedonia_negative_count_column <- 'no-anhedonia_negative_count'
   no_anhedonia_average_column <- 'no-anhedonia_average'
   no_anhedonia_sd_column <- 'no-anhedonia_sd'
 
@@ -160,14 +169,18 @@
   table_result['x', anhedonia_column] <- 0
   table_result['x', anhedonia_count_column] <- 0
   table_result['x', anhedonia_positive_column] <- 0
+  table_result['x', anhedonia_positive_count_column] <- 0
   table_result['x', anhedonia_negative_column] <- 0
+  table_result['x', anhedonia_negative_count_column] <- 0
   table_result['x', anhedonia_average_column] <- 0
   table_result['x', anhedonia_sd_column] <- 0
 
   table_result['x', no_anhedonia_column] <- 0
   table_result['x', no_anhedonia_count_column] <- 0
   table_result['x', no_anhedonia_positive_column] <- 0
+  table_result['x', no_anhedonia_positive_count_column] <- 0
   table_result['x', no_anhedonia_negative_column] <- 0
+  table_result['x', no_anhedonia_negative_count_column] <- 0
   table_result['x', no_anhedonia_average_column] <- 0
   table_result['x', no_anhedonia_sd_column] <- 0
 
@@ -183,27 +196,35 @@
       # Anhedonia
       an_total <- result_matrices$anhedonia$total[source, target]
       an_positive <- result_matrices$anhedonia$positive[source, target]
+      an_positive_count <- result_matrices$anhedonia$positive_count[source, target]
       an_negative <- result_matrices$anhedonia$negative[source, target]
+      an_negative_count <- result_matrices$anhedonia$negative_count[source, target]
       an_total_sd <- result_matrices$anhedonia[['total-sd']][[source, target]]
       an_total_sd <- an_total_sd[!is.na(an_total_sd) & an_total_sd != 0]
       table_result[row_name, anhedonia_column] <-  an_total
       table_result[row_name, anhedonia_count_column] <- length(an_total_sd)
       table_result[row_name, anhedonia_sd_column] <- sd(an_total_sd)
       table_result[row_name, anhedonia_positive_column] <-  an_positive
+      table_result[row_name, anhedonia_positive_count_column] <-  an_positive_count
       table_result[row_name, anhedonia_negative_column] <-  an_negative
+      table_result[row_name, anhedonia_negative_count_column] <-  an_negative_count
       table_result[row_name, anhedonia_average_column] <- mean(an_total_sd)
 
       # No-Anhedonia
       no_an_total <- result_matrices$no_anhedonia$total[source, target]
       no_an_positive <- result_matrices$no_anhedonia$positive[source, target]
+      no_an_positive_count <- result_matrices$no_anhedonia$positive_count[source, target]
       no_an_negative <- result_matrices$no_anhedonia$negative[source, target]
+      no_an_negative_count <- result_matrices$no_anhedonia$negative_count[source, target]
       no_an_total_sd <- result_matrices$no_anhedonia[['total-sd']][[source, target]]
       no_an_total_sd <- no_an_total_sd[!is.na(no_an_total_sd) & no_an_total_sd != 0]
       table_result[row_name, no_anhedonia_column] <-  no_an_total
       table_result[row_name, no_anhedonia_count_column] <- length(no_an_total_sd)
       table_result[row_name, no_anhedonia_sd_column] <- sd(no_an_total_sd)
       table_result[row_name, no_anhedonia_positive_column] <-  no_an_positive
+      table_result[row_name, no_anhedonia_positive_count_column] <-  no_an_positive_count
       table_result[row_name, no_anhedonia_negative_column] <-  no_an_negative
+      table_result[row_name, no_anhedonia_negative_count_column] <-  no_an_negative_count
       table_result[row_name, no_anhedonia_average_column] <- mean(no_an_total_sd)
 
       # Total
@@ -222,7 +243,7 @@
 export_total_effect_networks <- function(airas,general_groups) {
   networks_output <- mclapply(airas, .generate_total_effect_network, mc.cores = detectCores())
   groups <- c('other', 'anhedonia', 'no_anhedonia', 'total')
-  options <- c('total', 'total-sd', 'positive', 'negative')
+  options <- c('total', 'total-sd', 'positive', 'negative', 'positive_count', 'negative_count')
   names <- dimnames(airas[[1]]$var_model$y)[[2]]
 
   # Retrieve the number of variables
