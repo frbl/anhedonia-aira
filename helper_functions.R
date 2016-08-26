1d8c47258f006431b937e0303df6f4eb2a18580e
Frank Blaauw <frank.blaauw@gmail.com>
2016-08-26 19:42:49 +0200

    Styling

--- helper_functions.R
+++ helper_functions.R
@@ -21,34 +21,34 @@ std_dev_func <- function(x) sd(x, na.rm=TRUE)
 
 .create_standard_deviation_matrix <- function(resmat, labels) {
   # Standard deviation function to use
-  
+
   plottable <- .create_result_matrix(labels, dim(resmat)[2], default_value=0)
   for(r in 1:dim(plottable)[1])
     for(c in 1:dim(plottable)[2]){
       plottable[r,c] <- std_dev_func(resmat[r,c][[1]])
     }
-  
+
   plottable[is.na(plottable)] <- 0
   plottable
 }
 
 .create_result_table <- function(groups) {
   table_result <- data.frame()
-  
+
   # Create an x row with all columns. The x will be removed later on.
   table_result['x', 'Total'] <- 0
   table_result['x', 'Total-average'] <- 0
   table_result['x', 'Total-sd'] <- list(rep(NA,50))
-  
+
   for(group in groups) {
-    if(group == 'other') next    
+    if(group == 'other') next
     col_name <- paste(group, '(Negative edges)', sep=" ")
     avgcolname <- paste(col_name,'average',sep='-')
     sdcolname <- paste(col_name,'sd',sep='-')
     table_result['x', col_name] <- 0
     table_result['x', avgcolname] <- 0
     table_result['x', sdcolname] <- list(rep(NA,50))
-    
+
     col_name <- paste(group, '(Positive edges)', sep=" ")
     avgcolname <- paste(col_name,'average',sep='-')
     sdcolname <- paste(col_name,'sd',sep='-')
