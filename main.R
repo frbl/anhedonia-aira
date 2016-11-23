e36e791cfe7f4f8187b853f95603e7bbb11dc3f4
Frank Blaauw <frank.blaauw@gmail.com>
2016-11-23 10:08:43 +0100

    Remove creation of positive networks

--- main.R
+++ main.R
@@ -39,17 +39,14 @@ data_file <- "mad_diary_all_update19feb2015_merge_fionneke.csv"
 
 # Use autovar core?
 core <- TRUE
-run_all <- FALSE
 bootstrap_iterations <- 2000
-#use_100_min_value <- FALSE
-use_positive_model <- FALSE
 
+# These two lists contain ALL models, and will be sanitized in a later step
 no_anhedonia <- c(102232,104789,110514,110544,107110,109755,112244,111264,111884,105962,112450,110642,104703,
                   110375,111543,104229,109531,109747,110676,104255,109751,112186,105722,104231,106423)
 
 anhedonia <- c(104517,107596,108172,112028,100849,111939,101912,111745,111779,111464,111958,111473,111566,
                100713,111737,111778,110360,111184,111492,112052,105882,111459,110064,111268,111350)
-fail()
 
 main <- function() {
   # Time how long it takes
@@ -58,7 +55,7 @@ main <- function() {
   # Close any sinks if still open (because of a crash or kill in the previous run)
   suppressWarnings(sink())
 
-  # Remove all plots
+  # Remove all plots still on an output device
   if(!is.null(dev.list()) && !is.na(dev.list()['pdf'])) dev.off(dev.list()["pdf"])
   if(!is.null(dev.list()) && !is.na(dev.list()['RStudioGD'])) dev.off(dev.list()["RStudioGD"])
 
@@ -70,8 +67,6 @@ main <- function() {
 
   # Split the file into separate CSVs
   create_csvs(data_file=data_file, ids_to_keep = c(no_anhedonia, anhedonia))
-
-  # Generate var models for each individual. These are cached in the global 'res' variable.
   setwd('csv')
 
   # These columns will be removed before imputation (mainly because of linearity)
@@ -81,68 +76,16 @@ main <- function() {
   autovar_columns <- c( 'pa_activation', 'pa_deactivation', 'na_activation', 'na_deactivation', 'activity', 'upset')
   groups <- list(c('pa_activation', 'pa_deactivation'), c('na_activation', 'na_deactivation'), c('activity', 'upset'))
 
-  # These are the variables that will be inverted using AIRA
-  #negative_variables <- c( 'not_na_activation', 'not_na_deactivation', 'not_upset', 'na_activation', 'na_deactivation', 'upset', 'lnna_activation', 'lnna_deactivation', 'lnupset')
-  negative_variables <- c('not_pa_activation', 'not_pa_deactivation', 'not_activity', 'pa_activation', 'pa_deactivation', 'activity', 'lnpa_activation', 'lnpa_deactivation', 'lnactivity')
-
   files <- list.files()
   all_loaded_files <<- load_all_files(files, removed_columns = removed_columns)
   setwd("../")
 
-
-  #type <- ifelse(use_100_min_value, 'positive', 'normal')
-  type <- ifelse(use_positive_model, 'positive', 'normal')
+  type <- 'normal'
   dirname <- paste('output',type,format(Sys.time(), "%Y%m%d-%H_%M_%S"), sep='-')
   dir.create(dirname)
   setwd(dirname)
 
   create_descriptives_output(files = all_loaded_files, dirname = 'desciptives_imputed_data')
-
-  # If we want to create columns in which the value is 100 - the original value, we should enable the use_100_min_value option
-  # if(use_100_min_value) {
-  #   autovar_columns <- c( 'pa_activation', 'pa_deactivation', 'not_na_activation', 'not_na_deactivation', 'activity', 'not_upset')
-  #   for(i in 1:length(all_loaded_files)) {
-  #     data <- all_loaded_files[[i]]$raw_data
-  #
-  #     data['not_na_deactivation'] <- 100 - data['na_deactivation']
-  #     all_loaded_files[[i]]$data$multiple['not_na_deactivation'] <<- data['not_na_deactivation']
-  #
-  #     data['not_na_activation'] <- 100 - data['na_activation']
-  #     all_loaded_files[[i]]$data$multiple['not_na_activation'] <<- data['not_na_activation']
-  #
-  #     data['not_upset'] <- 100 - data['upset']
-  #     all_loaded_files[[i]]$data$multiple['not_upset'] <<- data['not_upset']
-  #
-  #     all_loaded_files[[i]]$raw_data <<- data
-  #     print(names(all_loaded_files[[i]]$raw_data))
-  #   }
-  #
-  #   create_descriptives_output(files = all_loaded_files, dirname = 'desciptives_positivized_imputed_data')
-  # }
-
-  # Change the variable names to NOT_.. if we have a positive model.
-  if(use_positive_model) {
-    autovar_columns <- c( 'not_pa_activation', 'not_pa_deactivation', 'na_activation', 'na_deactivation', 'not_activity', 'upset')
-    groups <- list(c('not_pa_activation', 'not_pa_deactivation'), c('na_activation', 'na_deactivation'), c('not_activity', 'upset'))
-    for(i in 1:length(all_loaded_files)) {
-      for (positive_name in negative_variables) {
-        data <- all_loaded_files[[i]]$raw_data
-
-        data['not_pa_deactivation'] <- data['pa_deactivation']
-        all_loaded_files[[i]]$data$multiple['not_pa_deactivation'] <<- data['not_pa_deactivation']
-
-        data['not_pa_activation'] <- data['pa_activation']
-        all_loaded_files[[i]]$data$multiple['not_pa_activation'] <<- data['not_pa_activation']
-
-        data['not_activity'] <- data['activity']
-        all_loaded_files[[i]]$data$multiple['not_activity'] <<- data['not_activity']
-
-        all_loaded_files[[i]]$raw_data <<- data
-        print(names(all_loaded_files[[i]]$raw_data))
-      }
-    }
-    create_descriptives_output(files = all_loaded_files, dirname = 'desciptives_positivized_imputed_data')
-  }
 
   # Res is a list containing the best model generated using autovar core.
   res <<- list()
@@ -153,27 +96,11 @@ main <- function() {
   total_res <<- calculate_all_files(files = all_loaded_files, autovar_columns = autovar_columns)
 
   # Select the best model from the 4 models generated by autovarcore
+  # Generate var models for each individual. These are cached in the global 'res' variable.
   for (models in total_res) {
     model <- models[[1]]
     model$name <- models$name
     res[[models$name]] <<- model
-  }
-
-  if(use_positive_model) {
-    # Convert the varmodels to positive models
-    print('Converting var models to positive models')
-    print(res[['110544.csv']]$varest$varresult$not_activity)
-    if(use_positive_model) {
-      new_res <- list()
-      for (model in res) {
-        if(is.null(model)) next
-        print(paste('Converting', model$name))
-        model$varest <- convert_var_to_positive(model$varest, negative_variables = negative_variables)
-        new_res[[model$name]] <- model
-      }
-      res <<- new_res
-    }
-    print(res[['110544.csv']]$varest$varresult$not_activity)
   }
 
   # Print the quality of the models
@@ -237,9 +164,6 @@ main <- function() {
   # This is one of the actual AIRA steps. Calculate which of the nodes has the most impact on the other nodes
   print('Determining best AIRA nodes for each model')
   airas <<- determine_best_aira_nodes_to_json(airas)
-
-  # This is one of the actual AIRA steps. Calculate which of the nodes has the most impact on the other nodes
-  #airas <<- determine_how_to_reduce_stress(airas)
 
   # Generate overview networks of the number of edges in the VAR models (NO IRF)
   print('Exporting total var edges')
