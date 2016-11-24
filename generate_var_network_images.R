4c15512df8c0daca4dac90654a6e1573d1c2690b
Frank Blaauw <frank.blaauw@gmail.com>
2016-11-24 11:18:12 +0100

    Use the general vsize and use circular layout for plotting

--- generate_var_network_images.R
+++ generate_var_network_images.R
@@ -1,4 +1,4 @@
-export_aira_var_network_graphs <- function(airas) {
+export_aira_var_network_graphs <- function(airas, name_mapping) {
   plots <- list()
   plot.new()
   glob_minimum <- 0
@@ -11,13 +11,14 @@ export_aira_var_network_graphs <- function(airas) {
     aira_output <- AiraOutput$new(aira = aira_model)
     var_network <- aira_output$export_var_network(autoregressive=TRUE)
     nms <- dimnames(var_network)[[2]]
-    plots[[aira_model$name]] <- qgraph(var_network, plot=TRUE, edge.labels = TRUE, vsize = 10, layout = "spring", posCol = "chartreuse3", labels = nms,
+    nms <- as.vector(unlist(name_mapping[nms]))
+    plots[[aira_model$name]] <- qgraph(var_network, plot=TRUE, edge.labels = TRUE, vsize = glob_vsize, layout = "circle", posCol = "chartreuse3", labels = nms,
                                        title=paste(group, '(', aira_model$name, ')'))
     i<- i + 1
   }
   layout <- averageLayout(plots)
   print('Exporting all plots')
   for(plot in plots) {
-    qgraph(plot, vsize=4.5, edge.labels = TRUE, minimum = glob_minimum, layout=layout, posCol="chartreuse3")#,nodeNames=bdinms2,legend.cex=0.6)
+    qgraph(plot, vsize=glob_vsize, edge.labels = TRUE, minimum = glob_minimum, layout=layout, posCol="chartreuse3")#,nodeNames=bdinms2,legend.cex=0.6)
   }
 }
