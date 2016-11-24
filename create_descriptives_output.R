1826bf0480431d40d1484772148d46378b229cb9
Frank Blaauw <frank.blaauw@gmail.com>
2016-11-24 11:14:27 +0100

    Added comments

--- create_descriptives_output.R
+++ create_descriptives_output.R
@@ -3,6 +3,8 @@ create_descriptives_output <- function(files, dirname) {
   setwd(dirname)
   for (file in all_loaded_files) {
     temp_name <- strsplit(file$real_file_name, "\\.")[[1]][1]
+    
+    # Create a summary of the data and write it to a file.
     sink(paste('summary_', temp_name, '.txt', sep=''))
     a <- summary(file$raw_data)
     print(a)
